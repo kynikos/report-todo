fde5d97a0d66a988dafaf0f418fc746b16e7090a
Dario Giovannetti <dev@dariogiovannetti.net>
2020-03-06 00:34:48 +0800

    Abstract commander setup into @kynikos/tasks

--- tasks.js
+++ tasks.js
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-sync,no-await-in-loop,no-use-before-define,no-console */
-const path = require('path')
 const fs = require('fs')
-const {emptyDirSync, copySync: xCopySync} = require('fs-extra')
 const process = require('process')
-const {spawnSync} = require('child_process')
-const readlineSync = require('readline-sync')
 const {oneLine: L} = require('common-tags')
-// TODO[setup]: minimist is a simpler alternative to commander.js
-const commander = require('commander')
 const {
   eslint,
 } = require('@kynikos/tasks/subprocess')
 const {
   linkSelf,
-  linkDependencies,
   maintainPackageDependencies,
 } = require('@kynikos/tasks/dependencies')
 const {jest} = require('@kynikos/tasks/testing')
@@ -29,86 +22,13 @@ const {
   npmPublish,
   submitToAur,
 } = require('@kynikos/tasks/releasing')
+const {wrapCommander} = require('@kynikos/tasks/commander')
 const {makeReadme} = require('./aux/README')
 const {reportTodo} = require('./src/index')
 const packageJson = require('./package.json')
 
-commander
-  .command('deps')
-  .description('run semi-automated dependency maintenance operations')
-  .action(() => maintainDependencies())
-
-commander
-  .command('lint')
-  .description('lint the source code')
-  .action(() => lint())
-
-commander
-  .command('test [REGEX]')
-  .description(L`run the automated tests; optionally only run tests with a name
-    that matches REGEX`)
-  .option('-v, --verbose', 'display all individual test results')
-  .option('-c, --print-console', 'let tests print messages through the console')
-  .option('-p, --print-received', L`print received test values for debugging
-    or updating the expected values after changing the tests (it implies
-    --print-console)`)
-  .option('-u, --update-expected', L`overwrite the expected test files with the
-    received values, useful after changing the tests`)
-  // eslint-disable-next-line jest/require-top-level-describe,jest/no-disabled-tests,jest/expect-expect,jest/valid-title
-  .action((regex, {verbose, printConsole, printReceived, updateExpected}) => {
-    runTests({
-      testNameRegex: regex,
-      verbose,
-      printConsole,
-      printReceived,
-      updateExpected,
-    })
-  })
-
-commander
-  .command('todo')
-  .description('generate a todo-report for this very program')
-  .action(() => todo())
-
-commander
-  .command('docs')
-  .description('build the documentation')
-  .action(() => docs())
-
-commander
-  .command('pkg-setup')
-  .description('set up the PKGBUILD file')
-  .action(() => setupPkg())
-
-commander
-  .command('pkg-make')
-  .description('make the PKGBUILD file')
-  .action(() => makePkg())
-
-commander
-  .command('pkg-install')
-  .description('install the Pacman tarball')
-  .option('--pkgrel <NUMBER>', "set the 'pkgrel' number", 1)
-  .action(({pkgrel}) => installPkg({pkgrel}))
-
-commander
-  .command('pub-npm')
-  .description('publish the package to the NPM repository')
-  .action(() => publishToNpm())
-
-commander
-  .command('pub-aur')
-  .description('publish the package to the AUR repository')
-  .action(() => publishToAur())
-
-commander
-  .command('release')
-  .description('build and release the application')
-  .action(() => release())
-
-commander.parse(process.argv)
-
 // TODO[setup]: The @kynikos dependencies should only provide peerDependencies
+
 
 function maintainDependencies() {
   maintainPackageDependencies(
@@ -253,3 +173,20 @@ function release() {
     // restoreDevelopmentEnvironment,
   })
 }
+
+
+const commander = wrapCommander({
+  maintainDependencies,
+  lint,
+  runTests,
+  todo,
+  docs,
+  setupPkg,
+  makePkg,
+  installPkg,
+  publishToNpm,
+  publishToAur,
+  release,
+})
+
+commander.parse(process.argv)
